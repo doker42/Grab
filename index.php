3a6ca2a64ec7bf34a56bb853d988b519e8ae71bc
doker42 <doker42@gmail.com>
2018-05-10 22:14:01 +0300

    start

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+
+$data = file_get_contents('https://blockchain.info/ticker');
+
+$decode_data = json_decode($data);
+
+$item = $decode_data->USD->last;
+
+$ar = [ $item ];
+
+$fp = fopen('/home/dok/projects/websites/file.csv', 'a+');
+
+fputcsv($fp, $ar);
+
+fclose($fp);
+
+
+
+
+
+
+
